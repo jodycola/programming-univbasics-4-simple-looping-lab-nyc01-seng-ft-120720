ea207a2a5e5f8142e46137e0fcfeb72d3c165ae0
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-23 01:29:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,3 +18,13 @@ def loop_message_n_times(message, number)
     counter += 1
   end
 end
+
+def output_array(array)
+  counter = 0
+  
+  while counter <= array.length
+  puts array[counter]
+  
+  counter += 1
+end
+end
